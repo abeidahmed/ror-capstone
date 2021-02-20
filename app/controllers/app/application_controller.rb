42d386bdd74d50e7afdebabc7e5252a53f5133b0
abeidahmed <abeidahmed92@gmail.com>
2021-02-20 12:56:55 +0530

    Make app namespace protected

--- app/controllers/app/application_controller.rb
+++ app/controllers/app/application_controller.rb
@@ -1,3 +1,11 @@
 class App::ApplicationController < ApplicationController
   layout 'app'
+
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    redirect_to new_session_path, alert: 'Please signup or login to continue' unless user_signed_in?
+  end
 end
