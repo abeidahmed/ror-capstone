d102be5b64f038df51a5b97730437459ef7b2ef3
abeidahmed <abeidahmed92@gmail.com>
2021-02-20 10:51:39 +0530

    Add sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(username: username)
+
+    if user&.authenticate(params[:password])
+      sign_in(user)
+    else
+      render json: { errors: { invalid: ['credentials'] } }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def username
+    params[:username].to_s.downcase
+  end
 end
