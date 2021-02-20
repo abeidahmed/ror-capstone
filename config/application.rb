d84c84d0dd9a01c65716c9eb5c4d77148db2142d
abeidahmed <abeidahmed92@gmail.com>
2021-02-20 09:59:35 +0530

    Disable generation of scss, helpers when using rails g

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,10 @@ module RorCapstone
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators.stylesheets  = false
+    config.generators.helper       = false
+    config.generators.helper_specs = false
+    config.generators.view_specs   = false
   end
 end
