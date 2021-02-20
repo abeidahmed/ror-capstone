270ed9661664c98525a7124a684291d80d671db3
abeidahmed <abeidahmed92@gmail.com>
2021-02-20 10:37:51 +0530

    Add user controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      sign_in(user)
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :full_name, :password)
+  end
 end
