881fdf93ac4d997bbb3fccdad5de2a7d0394279c
abeidahmed <abeidahmed92@gmail.com>
2021-02-22 18:53:19 +0530

    Seed db with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  user = User.create! username: Faker::Internet.unique.username, full_name: Faker::Name.name, password: 'super_secret'
+
+  5.times do
+    tweet = user.tweets.create! text: Faker::Quote.famous_last_words
+    tweet.liked_by(user)
+  end
+end
